f024f7c2e0fbd10f8b4e8963e968d5d3345df2f8
M_dgettMann <87083881+MidgettMann@users.noreply.github.com>
2022-02-28 19:25:11 +0000

    Update and rename GuiManager_[v1].lua to GuiManager_[v1.1].lua
    > Updated Gui:Destroy() and Gui:Clear() to destroy BindableEvents
    > Fixed local BindableEvents to not pass the ui's table as it's unnecessary
    > Removed Gui.BulkSetup(), Gui.BulkTween(), Gui.BulkChange() and Gui:Remove() as they're redundant
    > Added a new setting 'DisableAttributes' which stops attributes being set by the module
    > Some other changes for internal organisation

--- Modules/GuiManager_[v1.1].lua
+++ Modules/GuiManager_[v1.1].lua
@@ -28,7 +28,8 @@ type grid = {
 
 local Settings = {
 	DisableGlobalEvents = false,
-	DisableLocalEvents = false
+	DisableLocalEvents = false,
+	DisableAttributes = false
 }
 
 local Gui = {}
@@ -38,6 +39,7 @@ Gui.__type = "Gui"
 Gui.DraggingBegan = draggingBegan.Event
 Gui.DraggingChanged = draggingChanged.Event
 Gui.DraggingEnded = draggingEnded.Event
+
 Gui.RotatingBegan = rotatingBegan.Event
 Gui.RotatingChanged = rotatingChanged.Event
 Gui.RotatingEnded = rotatingEnded.Event
@@ -46,9 +48,38 @@ local function round(num : number, bound : number) : number
 	return math.round(num / bound) * bound
 end
 
-function Gui.new(name : string, props : tbl) : any
-	assert(type(name) == "string", "name must be a string")
+local function metaSetup(ui : Instance) : any
+	assert(typeof(ui) == "Instance", "ui must be an Instance")
 	
+	local events = {
+		draggingBegan = Instance.new("BindableEvent"),
+		draggingChanged = Instance.new("BindableEvent"),
+		draggingEnded = Instance.new("BindableEvent"),
+		
+		rotatingBegan = Instance.new("BindableEvent"),
+		rotatingChanged = Instance.new("BindableEvent"),
+		rotatingEnded = Instance.new("BindableEvent")
+	}
+	local tbl ={
+		["Ui"] = ui,
+		["Connections"] = {},
+		["BindableEvents"] = table.freeze(events),
+		["ScreenGui"] = ui:FindFirstAncestorWhichIsA("ScreenGui"),
+		
+		["DraggingBegan"] =  events.draggingBegan.Event,
+		["DraggingChanged"] = events.draggingChanged.Event,
+		["DraggingEnded"] = events.draggingEnded.Event,
+		
+		["RotatingBegan"] = events.rotatingBegan.Event,
+		["RotatingChanged"] = events.rotatingChanged.Event,
+		["RotatingEnded"] = events.rotatingEnded.Event
+	}
+	
+	assert(tbl.ScreenGui, "The ui must have a ScreenGui as an Ancestor")
+	return setmetatable(tbl, Gui)
+end
+
+function Gui.new(name : string, props : tbl) : tbl	
 	local ui = Instance.new(name)
 	local parent = nil
 	
@@ -58,127 +89,18 @@ function Gui.new(name : string, props : tbl) : any
 	end
 	
 	ui.Parent = parent
-	
-	local events = {
-		draggingBegan = Instance.new("BindableEvent"),
-		draggingChanged = Instance.new("BindableEvent"),
-		draggingEnded = Instance.new("BindableEvent"),
-		rotatingBegan = Instance.new("BindableEvent"),
-		rotatingChanged = Instance.new("BindableEvent"),
-		rotatingEnded = Instance.new("BindableEvent")
-	}
-	local tbl = {
-		["Ui"] = ui,
-		["Connections"] = {},
-		["BindableEvents"] = table.freeze(events),
-		["ScreenGui"] = ui:FindFirstAncestorWhichIsA("ScreenGui"),
-
-		["DraggingBegan"] =  events.draggingBegan.Event,
-		["DraggingChanged"] = events.draggingChanged.Event,
-		["DraggingEnded"] = events.draggingEnded.Event,
-		["RotatingBegan"] = events.rotatingBegan.Event,
-		["RotatingChanged"] = events.rotatingChanged.Event,
-		["RotatingEnded"] = events.rotatingEnded.Event
-	}
-	
-	assert(tbl.ScreenGui, "The ui must have a ScreenGui as an Ancestor")
-	
-	return setmetatable(tbl, Gui)
+	return metaSetup(ui)
 end
 
-function Gui.setup(ui : Instance) : any
-	assert(typeof(ui) == "Instance", "ui must be an Instance")
-	
-	local events = {
-		draggingBegan = Instance.new("BindableEvent"),
-		draggingChanged = Instance.new("BindableEvent"),
-		draggingEnded = Instance.new("BindableEvent"),
-		rotatingBegan = Instance.new("BindableEvent"),
-		rotatingChanged = Instance.new("BindableEvent"),
-		rotatingEnded = Instance.new("BindableEvent")
-	}
-	local tbl = {
-		["Ui"] = ui,
-		["Connections"] = {},
-		["BindableEvents"] = table.freeze(events),
-		["ScreenGui"] = ui:FindFirstAncestorWhichIsA("ScreenGui"),
-
-		["DraggingBegan"] =  events.draggingBegan.Event,
-		["DraggingChanged"] = events.draggingChanged.Event,
-		["DraggingEnded"] = events.draggingEnded.Event,
-		["RotatingBegan"] = events.rotatingBegan.Event,
-		["RotatingChanged"] = events.rotatingChanged.Event,
-		["RotatingEnded"] = events.rotatingEnded.Event
-	}
-	
-	assert(tbl.ScreenGui, "The ui must have a ScreenGui as an Ancestor")
-	
-	return setmetatable(tbl, Gui)
+function Gui.setup(ui : Instance) : tbl
+	return metaSetup(ui)
 end
 
 function Gui.updateSettings(setting : string, value : any)
 	Settings[setting] = value
 end
 
-function Gui.BulkSetup(uis : array) : array
-	local setups = {}
-	
-	for _, ui in ipairs(uis) do
-		assert(typeof(ui) == "Instance", "ui must be an Instance")
-		
-		local events = {
-			draggingBegan = Instance.new("BindableEvent"),
-			draggingChanged = Instance.new("BindableEvent"),
-			draggingEnded = Instance.new("BindableEvent"),
-			rotatingBegan = Instance.new("BindableEvent"),
-			rotatingChanged = Instance.new("BindableEvent"),
-			rotatingEnded = Instance.new("BindableEvent")
-		}
-		local tbl = {
-			["Ui"] = ui,
-			["Connections"] = {},
-			["BindableEvents"] = table.freeze(events),
-			["ScreenGui"] = ui:FindFirstAncestorWhichIsA("ScreenGui"),
-			
-			["DraggingBegan"] =  events.draggingBegan.Event,
-			["DraggingChanged"] = events.draggingChanged.Event,
-			["DraggingEnded"] = events.draggingEnded.Event,
-			["RotatingBegan"] = events.rotatingBegan.Event,
-			["RotatingChanged"] = events.rotatingChanged.Event,
-			["RotatingEnded"] = events.rotatingEnded.Event
-		}
-		
-		assert(tbl.ScreenGui, "The ui must have a ScreenGui as an Ancestor")
-		
-		table.insert(setups, setmetatable(tbl, Gui))
-	end
-	
-	return setups
-end
-
-function Gui.BulkTween(uis : array, goal : tbl, tweenInfo : TweenInfo?)
-	assert(type(uis) == "table" and uis[1], "uis must be an array")
-	assert(type(goal) == "table", "goal must be a table")
-	tweenInfo = (tweenInfo or defaultInfo)
-	
-	for _, ui in ipairs(uis) do
-		tweenService:Create(ui, tweenInfo, goal):Play()
-	end
-end
-
-function Gui.BulkChange(uis : array, goal : tbl)
-	assert(type(uis) == "table" and uis[1], "uis must be an array")
-	assert(type(goal) == "table", "goal must be a table")
-	
-	for _, ui : any in ipairs(uis) do
-		for prop, value in pairs(goal) do
-			ui[prop] = value
-		end
-	end
-end
-
 function Gui:Tween(goal : tbl, tweenInfo : TweenInfo?)
-	assert(type(goal) == "table", "goal must be a table")
 	tweenInfo = (tweenInfo or defaultInfo)
 	
 	tweenService:Create(self.Ui, tweenInfo, goal):Play()
@@ -193,9 +115,7 @@ function Gui:TweenChildren(goal : tbl, tweenInfo : TweenInfo?, recursive : boole
 	end
 end
 
-function Gui:Change(goal : tbl)
-	assert(type(goal) == "table", "goal must be a table")
-	
+function Gui:Change(goal : tbl)	
 	for prop, value in pairs(goal) do
 		self.Ui[prop] = value
 	end
@@ -214,19 +134,28 @@ end
 
 function Gui:Destroy()
 	self.Ui:Destroy()
-	table.clear(self)
-end
-
-function Gui:Remove() : Instance
-	local ui = self.Ui
-	ui:Remove()
+	
+	for _, conn in pairs(self.Connections) do
+		if conn.Connected then conn:Disconnect() end
+	end
+	
+	for _, event in pairs(self.BindableEvents) do
+		event:Destroy()
+	end
 	
 	table.clear(self)
-	return ui
 end
 
 function Gui:Clear() : Instance
 	local ui = self.Ui
+	
+	for _, conn in pairs(self.Connections) do
+		if conn.Connected then conn:Disconnect() end
+	end
+	
+	for _, event in pairs(self.BindableEvents) do
+		event:Destroy()
+	end
 	
 	table.clear(self)
 	return ui
@@ -236,9 +165,9 @@ function Gui:Draggable(clamps : clamp?, grids : grid?)
 	assert(typeof(self.Ui.Parent) == "Instance" and self.Ui.Parent:IsA("GuiBase2d"), "The Ui's parent must be a GuiBase2d")
 	assert(self.Connections.Rotating == nil, "Rotation must be disabled before Dragging can be enabled")
 	
-	local dragging = self.Connections.Dragging
+	local draggingConn = self.Connections.Dragging
 	
-	if dragging then
+	if draggingConn then
 		if (self.Connections.DraggingBegan and self.Connections.DraggingBegan.Connected) then self.Connections.DraggingBegan:Disconnect() end
 		if (self.Connections.DraggingEnded and self.Connections.DraggingEnded.Connected) then self.Connections.DraggingEnded:Disconnect() end
 		if (self.Connections.Dragging and self.Connections.Dragging.Connected) then self.Connections.Dragging:Disconnect() end
@@ -247,17 +176,21 @@ function Gui:Draggable(clamps : clamp?, grids : grid?)
 		self.Connections.DraggingEnded = nil
 		self.Connections.Dragging = nil
 	else
+		local dragging = false
+		
 		self.Connections.DraggingBegan = self.Ui.InputBegan:Connect(function(input : InputObject, gp : boolean)
 			if not gp and (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then
-				self.Ui:SetAttribute("Dragging", true)
+				dragging = true
 				
 				if not Settings.DisableGlobalEvents then draggingBegan:Fire(self) end
-				if not Settings.DisableLocalEvents then self.BindableEvents.draggingBegan:Fire(self) end
+				if not Settings.DisableLocalEvents then self.BindableEvents.draggingBegan:Fire() end
+				if not Settings.DisableAttributes then self.Ui:SetAttribute("Dragging", true) end
 			end
 		end)
 		
 		self.Connections.DraggingEnded = self.Ui.InputEnded:Connect(function(input : InputObject, gp : boolean)
 			if not gp and (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then
+				dragging = false
 				
 				local absolutePos = self.Ui.Parent.AbsolutePosition
 				local mousePos = Vector2.new(input.Position.X - absolutePos.X, input.Position.Y - absolutePos.Y)
@@ -274,16 +207,15 @@ function Gui:Draggable(clamps : clamp?, grids : grid?)
 				scale = if clamps then UDim2.fromScale(clampX, clampY) else scale
 				
 				self.Ui.Position = scale
-				self.Ui:SetAttribute("Dragging", false)
 				
 				if not Settings.DisableGlobalEvents then draggingEnded:Fire(self) end
-				if not Settings.DisableLocalEvents then self.BindableEvents.draggingEnded:Fire(self) end
+				if not Settings.DisableLocalEvents then self.BindableEvents.draggingEnded:Fire() end
+				if not Settings.DisableAttributes then self.Ui:SetAttribute("Dragging", false) end
 			end
 		end)
 		
 		self.Connections.Dragging = UIS.InputChanged:Connect(function(input : InputObject, gp : boolean)
-			if not gp and (input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch) then
-				if not self.Ui:GetAttribute("Dragging") then return end
+			if dragging and not gp and (input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch) then
 				
 				local absolutePos = self.Ui.Parent.AbsolutePosition
 				local mousePos = Vector2.new(input.Position.X - absolutePos.X, input.Position.Y - absolutePos.Y)
@@ -302,7 +234,7 @@ function Gui:Draggable(clamps : clamp?, grids : grid?)
 				self.Ui.Position = scale
 				
 				if not Settings.DisableGlobalEvents then draggingChanged:Fire(self) end
-				if not Settings.DisableLocalEvents then self.BindableEvents.draggingChanged:Fire(self) end
+				if not Settings.DisableLocalEvents then self.BindableEvents.draggingChanged:Fire() end
 			end
 		end)
 	end
@@ -312,9 +244,9 @@ function Gui:Rotatable(clamp : Vector2?, segment : number?, OnlyOnEnded : boolea
 	assert(typeof(self.Ui.Parent) == "Instance" and self.Ui.Parent:IsA("GuiBase2d"), "The Ui's parent must be a GuiBase2d")
 	assert(self.Connections.Dragging == nil, "Dragging must be disabled before Rotation can be enabled")
 	
-	local rotating = self.Connections.Rotating
+	local rotatingConn = self.Connections.Rotating
 	
-	if rotating then
+	if rotatingConn then
 		if (self.Connections.RotatingBegan and self.Connections.RotatingBegan.Connected) then self.Connections.RotatingBegan:Disconnect() end
 		if (self.Connections.RotatingEnded and self.Connections.RotatingEnded.Connected) then self.Connections.RotatingEnded:Disconnect() end
 		if (self.Connections.Rotating and self.Connections.Rotating.Connected) then self.Connections.Rotating:Disconnect() end
@@ -323,18 +255,21 @@ function Gui:Rotatable(clamp : Vector2?, segment : number?, OnlyOnEnded : boolea
 		self.Connections.RotatingEnded = nil
 		self.Connections.Rotating = nil
 	else
+		local rotating = false
+		
 		self.Connections.RotatingBegan = self.Ui.InputBegan:Connect(function(input : InputObject, gp : boolean)
 			if not gp and (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then
-				self.Ui:SetAttribute("Rotating", true)
+				rotating = true
 				
 				if not Settings.DisableGlobalEvents then rotatingBegan:Fire(self) end
-				if not Settings.DisableLocalEvents then self.BindableEvents.rotatingBegan:Fire(self) end
+				if not Settings.DisableLocalEvents then self.BindableEvents.rotatingBegan:Fire() end
+				if not Settings.DisableAttributes then self.Ui:SetAttribute("Rotating", true) end
 			end
 		end)
 		
 		self.Connections.RotatingEnded = self.Ui.InputEnded:Connect(function(input : InputObject, gp : boolean)
 			if not gp and (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then
-				self.Ui:SetAttribute("Rotating", false)
+				rotating = false
 				
 				local absolutePos = self.ScreenGui.AbsolutePosition
 				local mousePos = Vector2.new(input.Position.X - absolutePos.X, input.Position.Y - absolutePos.Y)
@@ -348,13 +283,13 @@ function Gui:Rotatable(clamp : Vector2?, segment : number?, OnlyOnEnded : boolea
 				self.Ui.Rotation =  rotation
 				
 				if not Settings.DisableGlobalEvents then rotatingEnded:Fire(self) end
-				if not Settings.DisableLocalEvents then self.BindableEvents.rotatingEnded:Fire(self) end
+				if not Settings.DisableLocalEvents then self.BindableEvents.rotatingEnded:Fire() end
+				if not Settings.DisableAttributes then self.Ui:SetAttribute("Rotating", false) end
 			end
 		end)
 		
 		self.Connections.Rotating = UIS.InputChanged:Connect(function(input : InputObject, gp : boolean)
-			if not gp and (input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch) then
-				if not self.Ui:GetAttribute("Rotating") then return end
+			if rotating and not gp and (input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch) then
 				
 				local absolutePos = self.ScreenGui.AbsolutePosition
 				local mousePos = Vector2.new(input.Position.X - absolutePos.X, input.Position.Y - absolutePos.Y)
@@ -368,7 +303,7 @@ function Gui:Rotatable(clamp : Vector2?, segment : number?, OnlyOnEnded : boolea
 				self.Ui.Rotation =  rotation
 				
 				if not Settings.DisableGlobalEvents then rotatingChanged:Fire(self) end
-				if not Settings.DisableLocalEvents then self.BindableEvents.rotatingChanged:Fire(self) end
+				if not Settings.DisableLocalEvents then self.BindableEvents.rotatingChanged:Fire() end
 			end
 		end)
 	end
@@ -378,28 +313,24 @@ return Gui
 
 --[[
 Made by M_dgettMann (shadowflame63)
-GuiManager [v1] (Testing versions)
-                                    
+GuiManager [v1.1]
+
 How to use each basic function:
 	Gui.new(name, props, parent) -- Creates the named ui, sets it properties and sets up the metatable
 	Gui.setup(ui) -- Sets up a metatable so all these functions can be used on the passed ui Instance
 	Gui.updateSettings(setting, value) -- Sets the passed setting to the passed value
-	
-	Gui.BulkSetup(uis) -- Sets up all the passed ui Instances in a for loop, returns a table of the setup ui
-	Gui.BulkTween(uis, goal, tweenInfo) -- Tweens multiple ui instances with the given properties and values inside goal
-	Gui.BulkChange(uis, goal) -- Sets ui instances properties with the given properties and values inside goal, without tweening
+
 	Gui:Tween(goal, tweenInfo) -- Tween the values of the given properties inside goal
 	Gui:TweenChildren(goal, tweenInfo, recursive) -- Same as Tween, but includes children (or descendants if recursive is true)
 	Gui:Change(goal) -- Sets the values of the given properties inside goal, without tweening
 	Gui:ChangeChildren(goal, recursive) -- Same as Set, but includes children (or descendants if recursive is true)
 	
-	Gui:Destroy() -- Destroys the ui and clears the table
-	Gui:Remove() -- Removes the ui and clears the table, returns ui Instance
-	Gui:Clear() -- Clears the table, without destroying or removing the ui, returns ui Instance
+	Gui:Destroy() -- Destroys the ui and clears its associated table
+	Gui:Clear() -- Clears the ui's associated table without destroying the ui itself, returns ui Instance
 	
 How to use the global events:
 	Gui.DraggingBegan:Connect(function(ui) -- All ui fire this event, useful for knowing everything that is active in one go
-		print(ui) -- Prints the table that contains the Ui, it's active Connections and ScreenGui that it's under
+		print(ui) -- Prints the table that contains the Ui, it's active Connections and the ScreenGui it's under
 	end)
 	
 	> This example applies for DraggingBegan, DraggingChanged, DraggingEnded, RotatingBegan, RotatingChanged and RotatingEnded
@@ -428,4 +359,11 @@ How to use each advanced function:
 			segment -> number,
 			OnlyOnEnded -> boolean
 		)
+		
+How Setttings work:
+	Gui.updateSettings(setting, value) -- Changes the passed setting to the passed value
+	
+	> DisableGlobalEvents -> When true, all global bindable events associated with gui won't fire
+	> DisableLocalEvents -> When true, all local bindable events associated with gui won't fire
+	> DisableAttributes -> When true, all attributes made by this module won't be set on gui
 ]]
